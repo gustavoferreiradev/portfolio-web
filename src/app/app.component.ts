8039e35db2660c31b0c697917b16f7e55ce53d89
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2023-03-19 21:21:04 -0300

    Front End desenvolvido com Angular finalizado

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ContactsService } from './services/contacts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  name!: string;
+  email!: string;
+  subject!: string;
+  message!: string;
+
+
+  constructor(
+    private contactService: ContactsService,
+    private toastService: ToastrService
+  ) { }
+
+  sendMessage() {
+    this.contactService.addContact({
+      Name: this.name,
+      Email: this.email,
+      Subject: this.subject,
+      Message: this.message,
+    }).subscribe(() => {
+      this.toastService.success('Mensagem enviada', 'Sucesso');
+      this.name = ''
+      this.email = ''
+      this.subject = ''
+      this.message = ''
+    })
+  }
+
+}
