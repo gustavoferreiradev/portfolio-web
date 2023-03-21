e6354bea4d82f0a597262c2daf5106a44d98d6be
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2023-03-21 09:33:18 -0300

    Finalizada a aplicação

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -6,7 +6,8 @@ import Contact from '../models/contact';
   providedIn: 'root'
 })
 export class ContactsService {
-  apiUrl = "https://localhost:7074/contacts"
+  // apiUrl = "https://localhost:7074/contacts"
+  apiUrl = "https://portfolio-api-app-gustavo.azurewebsites.net/contacts"
   constructor(private http: HttpClient) {}
 
     addContact(contact: Contact) {
